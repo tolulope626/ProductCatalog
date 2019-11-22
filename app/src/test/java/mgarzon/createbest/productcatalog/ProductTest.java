95386acff508b0bbd87b600148f7692831506e8a
Tolulope Egbetokun <55300090+tolulope626@users.noreply.github.com>
2019-11-22 18:15:22 -0500

    Update ProductTest.java

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -6,21 +6,21 @@ public class ProductTest {
 
     @Test
     public void checkProductName() {
-        Product aProduct = new Product("1", "DELL MONITOR", 0);
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
         assertEquals("Check the name of the product", "DELL MONITOR", aProduct.getProductName());
     }
 
     @Test
     public void checkProductId() {
-        Product aProduct = new Product("1", "DELL MONITOR", 0);
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
         assertEquals("Check the id of the product", "1", aProduct.getId());
     }
 
     @Test
     public void checkProductPrice() {
         // I am making this test to fail
-        Product aProduct = new Product("1", "DELL MONITOR", 0);
-        assertEquals("Check the price of the product", "0", aProduct.getPrice());
+        Product aProduct = new Product("1", "DELL MONITOR", 180);
+        assertEquals("Check the price of the product", "180", aProduct.getPrice());
     }
 
 
